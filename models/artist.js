bcda134ff1aa06697b3e5d5177b72aaea39bfcf8
Clayton Roop <claytonroop@gmail.com>
2018-06-13 21:27:01 -0400

    save DB

---
+++ models/artist.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const artistSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipcode: { type: String, required: true },
+  facebook: { type: String },
+  instagram: { type: String },
+  phone: { type: String },
+  biography: String,
+  date: { type: Date, default: Date.now }
+});
+
+const Artist = mongoose.model("Artist", artistSchema);
+
+module.exports = Artist;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,12 +15,14 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-
+// app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/OnTheWall");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
