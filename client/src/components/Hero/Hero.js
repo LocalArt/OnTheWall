e6346dfe582dcdc8af89ac4260801dde8fc28497
andy.heymann@gmail.com <andy.heymann@gmail.com>
2018-06-14 20:29:58 -0400

    seachbar in

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -3,13 +3,14 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
   <div className="jumbotron text-center">
-    <h1>React Recipes</h1>
+    <h1> {artistName} </h1>
+    <h2> {artistCity}, {artistState} </h2>
     <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="http://www.recipepuppy.com/about/api/"
+    //   target="_blank"
+    //   rel="noopener noreferrer"
+    //   href="http://www.recipepuppy.com/about/api/"
     >
-      Powered by Recipe Puppy
+    //   Powered by Recipe Puppy
     </a>
   </div>
 );
