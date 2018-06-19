04ae6427a1ecafb8e7b9329146516ffcef236c7d
Esterling Accime <esterling@brainbytescreative.com>
2018-06-18 20:54:35 -0400

    fix: firebase integration

--- client/src/components/Upload/Upload.js
+++ client/src/components/Upload/Upload.js
@@ -1,46 +1,103 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import * as firebase from 'firebase';
 import "./Upload.css";
-import "../Firebase/Firebase";
+import { storage, database } from "../Firebase/Firebase";
 import axios from 'axios';
 
 class Upload extends React.Component {
-    state = {
-      selectedFile: null
+  state = {
+    selectedFile: null,
+    url: ''
+  }
+
+  componentDidMount = async function () {
+
+    const { setState } = this;
+    var storageRef = storage().ref('');
+    const data = await storageRef.child('artists/categories/Screen Shot 2018-05-19 at 11.10.43 AM.png').getDownloadURL();
+
+    this.setState({ url: data });
+  };
+
+
+  uploadProgress = (snapshot) => {
+    // TODO
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    };
+  }
+
+  uploadError = (error) => {
+    // A full list of error codes is available at
+    // https://firebase.google.com/docs/storage/web/handle-errors
+    switch (error.code) {
+      case 'storage/unauthorized':
+        // User doesn't have permission to access the object\
+        console.log('User do not have permission');
+        break;
+
+      case 'storage/canceled':
+        // User canceled the upload
+        console.log('User canceled the upload');
+        break;
+
+      case 'storage/unknown':
+        // Unknown error occurred, inspect error.serverResponse
+        console.log('Unknown error occurred, inspect error.serverResponse');
+        break;
     }
-  
-    // event listener for choosing the file
-    fileSelectedHandler = event => {
-      this.setState({
-        selectedFile: event.target.files[0]
-      })
-    }
-  
-    //event listenener for file upload
-    fileUploadHandler = () => {
-        // Default Javascript Object
-        const fd = new FormData();
-        fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
-        axios.post('', fd) 
-            // outputs any response
-            .then(res => {
-            console.log(res);
-        })
-            //catches an error
-            .catch(err => {
-            console.log(`${err}`)
-        });
-    }
-  
-    render() {
-        return (
-          <div className="Image">
-            <input type="file" onChange={this.fileSelectedHandler} ref={fileInput => this.fileInput = fileInput}/>
-            {/*<button onClick={() => this.fileInput.click()}>Pick File</button>*/}
-            <button onClick={this.fileUploadHandler}>Upload</button>
-          </div>
-        );
-    }
- }
+  };
+
+  // event listener for choosing the file
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+
+  //event listenener for file upload
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
+
+    const file = this.state.selectedFile; // file name
+    const metadata = {
+      contentType: 'image/jpeg'
+    };
+
+    // .ref(/artists/ + 'categories' + 'accimeesterlin');
+    const storageRef = storage().ref('artists/' + 'categories'); // ISSUE
+    const uploadTask = storageRef.child(file.name).put(file, metadata);
+    uploadTask.on(storage.TaskEvent.STATE_CHANGED, this.uploadProgress, this.uploadError, function () {
+      // Upload completed successfully, now we can get the download URL
+      uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+        console.log('File available at', downloadURL);
+      });
+    });
+
+  }
+
+  render() {
+    console.log('State: ', this.state);
+    return (
+      <div className="Image">
+        <input type="file" onChange={this.fileSelectedHandler} ref={fileInput => this.fileInput = fileInput} />
+        {/*<button onClick={() => this.fileInput.click()}>Pick File</button>*/}
+        <button onClick={this.fileUploadHandler}>Upload</button>
+
+        <img src={this.state.url} alt="" />
+      </div>
+    );
+  }
+}
 
 export default Upload;
