cfd501e6d7f220aa47a04e33a3239f5246722126
andy.heymann@gmail.com <andy.heymann@gmail.com>
2018-06-23 11:35:44 -0400

    thisisnice

--- client/src/components/ArtModalBootstrap/index.js
+++ client/src/components/ArtModalBootstrap/index.js
@@ -1,1 +1,1 @@
-export { default } from "./ArtModal";
+export { default } from "./ArtModalBootstrap";
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,27 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Card.css";
+import ArtistModal from "../ArtModalBootstrap";
 
-const Card = props => (
-  <div className="card">
-    {console.log("props.id= " + JSON.stringify(props))}
-    <img
-      className="card-img-top"
-      src="https://dg19s6hp6ufoh.cloudfront.net/pictures/613209425/large/PicassoBullsHead.jpeg?1491535886"
-      alt="Card image cap"
-    />
+class Card extends React.Component {
+  state = {
+    show: false
+  };
 
-    <div className="card-body">
-      <h5 className="card-artist">{props.name}</h5>
-      <p className="card-location">
-        {props.city}, {props.state}
-      </p>
-      <p className="card-title">Bull's Head</p>
-      <button href="#" className="btn btn-primary btn-card">
-        <Link to={"/artist/" + props.id}>Artist Bio</Link>
-      </button>
-    </div>
-  </div>
-);
+  handleToggle = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  render() {
+    return (
+      <div className="card">
+        <img
+          className="card-img-top"
+          src="https://dg19s6hp6ufoh.cloudfront.net/pictures/613209425/large/PicassoBullsHead.jpeg?1491535886"
+          alt="Card image cap"
+        />
+
+        <div className="card-body">
+          <h5 className="card-artist">{this.props.name}</h5>
+          <p className="card-location">
+            {this.props.city}, {this.props.state}
+          </p>
+          <p className="card-title">Bull's Head</p>
+          <button href="#" className="btn btn-primary btn-card">
+            <Link to={"/artist/" + this.props.id}>Artist Bio</Link>
+          </button>
+          <button onClick={this.handleToggle}>Modal</button>
+          <ArtistModal
+            handleToggle={this.handleToggle}
+            show={this.state.show}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Card;
