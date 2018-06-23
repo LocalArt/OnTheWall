eb35b9791637ec751d40f5b116c81de193e65885
Bob <melvirp@yahoo.com>
2018-06-23 10:48:24 -0400

    aboutproj folder, page, & about comp on venue page

---
+++ client/src/pages/aboutproj/aboutproj.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "../../components/About";
+
+const OurStory = () => (
+  <div>
+    <h1>HOLD FOR AboutProj PAGE YO!!!!!!!</h1>
+    <About
+      title={"FlipArtWall Production"}
+      bioscription={"By Andy, Bob, Clay, Marina, and Muhammad"}
+    />
+  </div>
+);
+
+export default OurStory;
