28d5370c17680bdc25519ff73c51b30ae10060c1
andy.heymann@gmail.com <andy.heymann@gmail.com>
2018-06-23 11:35:44 -0400

    thisisnice

--- client/src/components/ArtModalBootstrap/ArtModalBootstrap.js
+++ client/src/components/ArtModalBootstrap/ArtModalBootstrap.js
@@ -1,26 +1,35 @@
 import React from "react";
-
-import ModalWrapper from "../ModalWrapper.jsx";
+import { Modal, Button } from "react-bootstrap";
+import "./ArtModalBootstrap.css";
 
 const ArtModalBootstrap = props => {
-  <div className="static-modal">
-    <Modal.Dialog>
-      <Modal.Header>
-        <img
-          className="art-fullsize"
-          src="https://dg19s6hp6ufoh.cloudfront.net/pictures/613209425/large/PicassoBullsHead.jpeg?1491535886"
-          alt="full size image"
-        />
-      </Modal.Header>
+  console.log(props);
+  return (
+    <Modal
+      show={props.show}
+      animation={false}
+      onHide={props.handleToggle}
+      bsSize="large"
+      aria-labelledby="contained-modal-title-lg"
+    >
+      <Modal.Dialog>
+        <Modal.Header>
+          <img
+            className="art-fullsize"
+            src="https://dg19s6hp6ufoh.cloudfront.net/pictures/613209425/large/PicassoBullsHead.jpeg?1491535886"
+            alt="full size image"
+          />
+        </Modal.Header>
 
-      <Modal.Body>This is nice</Modal.Body>
+        <Modal.Body>This is nice</Modal.Body>
 
-      <Modal.Footer>
-        <Button>Close</Button>
-        <Button bsStyle="primary">Save changes</Button>
-      </Modal.Footer>
-    </Modal.Dialog>
-  </div>;
+        <Modal.Footer>
+          <Button onClick={props.handleToggle}>Close</Button>
+          <Button bsStyle="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </Modal>
+  );
 };
 
 export default ArtModalBootstrap;
